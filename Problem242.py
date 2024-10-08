5bb805fc98bcf546ed9e00b6de1d642a0a04b1c3
Parth Sharma <parthsharma@csu.fullerton.edu>
2024-10-08 15:42:48 -0700

    problem 242

--- Problem242.py
+++ Problem242.py
@@ -25,3 +25,16 @@ class Solution:
             return True
         return False
         
+  
+  # # Neetcode Solution
+  # class Solution:
+  #   def isAnagram(self, s: str, t: str) -> bool:
+  #       if len(s) != len(t):
+  #           return False
+
+  #       countS, countT = {}, {}
+
+  #       for i in range(len(s)):
+  #           countS[s[i]] = 1 + countS.get(s[i], 0)
+  #           countT[t[i]] = 1 + countT.get(t[i], 0)
+  #       return countS == countT
